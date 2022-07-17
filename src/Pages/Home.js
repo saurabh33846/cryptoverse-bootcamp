a1c30e8e79b81dd5ea47e9e1c5ad6e24bc3bec8a
saroj.kumar2@myntra.com <saroj.kumar2@myntra.com>
2022-07-17 06:15:33 +0530

    Part 3 exercise initial commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,27 +6,25 @@ import News from "../MockData/TopNews.json";
 import NewsCard from "../Components/NewsCard";
 import Exchanges from '../MockData/exchanges.json';
 import ExchangeCard from "../Components/Exchanges";
+import { fetchCoins } from "../Services/fetchCoins";
+import { fetchNews } from "../Services/fetchNews";
 
 export default function Home() {
   const [cards, setCards] = useState(Coins.coins);
-  const [news, setNews ] = useState(News.value);
+  const [news, setNews] = useState(News.value);
   const [exchanges, setExchanges] = useState(Exchanges.exchanges);
-  console.log("Exchange is ****",exchanges)
+
   useEffect(() => {
-        // fetch("https://coinranking1.p.rapidapi.com/coins?limit=10", {
-    //   headers: {
-    //     "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
-    //     "X-RapidAPI-Host": "coinranking1.p.rapidapi.com"
-    //   }
-    // })
-    //   .then((resp) => {
-    //     return resp.json();
-    //   })
-    //   .then((jsonResp) => {
-    //     console.log(jsonResp);
-    //   });
-  }, []);
-  
+    getCoinsAndNewsData()
+  }
+    , []);
+
+  const getCoinsAndNewsData = async () => {
+    // let coinsData = await fetchCoins();
+    // let newsData = await fetchNews();
+    // setCards(coinsData.data.coins)
+  }
+
   return (
     <div className={styles.container} >
       <div className={styles.container}>
@@ -46,47 +44,47 @@ export default function Home() {
         })}{" "}
       </div>
 
-      <div style={{padding:"8px 16px"}}>
+      <div style={{ padding: "8px 16px" }}>
         <h2>Top News </h2>
       </div>
 
-      <div className={styles.container}>       
+      <div className={styles.container}>
         {
-          news.map((oneNews)=>{
-            const {name,description, datePublished} = oneNews;
+          news.map((oneNews) => {
+            const { name, description, datePublished } = oneNews;
             const imageUrl = oneNews?.image?.thumbnail?.contentUrl;
             const thumbNailURL = oneNews?.provider[0]?.image?.thumbnail?.contentUrl;
             const providerName = oneNews?.provider[0]?.name;
-            return <NewsCard heading={name} 
-            imageUrl={imageUrl} 
-            description={description} 
-            thumbNailURL= {thumbNailURL} 
-            providerName={providerName}
-            datePublished = {datePublished}
+            return <NewsCard heading={name}
+              imageUrl={imageUrl}
+              description={description}
+              thumbNailURL={thumbNailURL}
+              providerName={providerName}
+              datePublished={datePublished}
             />
           })
         }
       </div>
 
-      <div style={{padding:"8px 16px"}}>
+      <div style={{ padding: "8px 16px" }}>
         <h2>Exchanges </h2>
       </div>
 
-      <div className={styles.container}>       
+      <div className={styles.container}>
         {exchanges.map((exchange) => {
-            return (
-              <ExchangeCard
-                name={exchange.name}
-                iconUrl={exchange.iconUrl}
-                price={exchange.price}
-                numberOfMarkets={exchange.numberOfMarkets}
-                change={exchange.change}
-                key={exchange.uuid}
-                id={exchange.uuid}
-                number={exchange.rank}
-              />
-            );
-          })}
+          return (
+            <ExchangeCard
+              name={exchange.name}
+              iconUrl={exchange.iconUrl}
+              price={exchange.price}
+              numberOfMarkets={exchange.numberOfMarkets}
+              change={exchange.change}
+              key={exchange.uuid}
+              id={exchange.uuid}
+              number={exchange.rank}
+            />
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/Services/utils.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const cryptoApiHeaders = {
+	'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+	'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+};
+
+export const newsApiHeaders = {
+	"X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_KEY,
+	"X-RapidAPI-Host": process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST_NEWS
+}
